315ba9e115312132a80d66f4c9bd11d5fa9a2b72
Malek <mmalek1421@gmail.com>
2017-01-10 18:17:18 -0500

    attempting to fix cors issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,7 +9,7 @@ export const DELETE_TICKER = 'DELETE_TICKER';
 export const FETCH_CHART = 'FETCH_CHART';
 
 export function fetchTicker(ticker) {
-  const url = `${ROOT_URL}?symbol=${ticker}`;
+  const url = `${ROOT_URL}?symbol=${ticker}&callback=myFunction`;
   const request = axios.get(url);
 
   return (dispatch) => {
@@ -22,7 +22,7 @@ export function fetchTicker(ticker) {
 }
 
 export function fetchChart(ticker) {
-  const selectedUrl = `${SELECT_URL}${ticker.Symbol}`
+  const selectedUrl = `${SELECT_URL}${ticker.Symbol}&callback=myFunction`
   const new_request = axios.get(selectedUrl);
 
   return (dispatch) => {
